6e727507120d0e9c8ebe646bdd60bfedb2087402
ukemike <ukemike4@gmail.com>
2023-02-13 16:21:42 +0100

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import 'lightgallery/scss/lg-zoom.scss';
 import type { AppProps } from 'next/app'
 import store from '../src/redux/store'
 import { Provider } from 'react-redux'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-KDKEJXSEFQ" />
+
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+    >
+      {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+  
+        gtag('config', 'G-KDKEJXSEFQ');`
+      }
+    </Script>
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
